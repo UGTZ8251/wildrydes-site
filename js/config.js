d4fb36e79cc65cc04f131b7cf99f812d531eec48
UGTZ8251 <phrouk@gmail.com>
2025-03-19 10:39:19 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_VeG88d4y0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '461e78dmvtpvpvp86f4pcmcn8d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_wyUhJGnHH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '28ep3p5rqrmm21ugp1rvhk3om5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
